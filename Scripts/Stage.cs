dec36d88e76b3af007dc28bf81a954c3c478bd0c
Dasom0108 <109723488+Dasom0108@users.noreply.github.com>
2023-09-25 11:04:07 +0900

    테트리스 이동 및 생성
    타일 하나만 가지고 생성하는 방법임

---
+++ Scripts/Stage.cs
@@ -0,0 +1,251 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    //필요 소스 불러오기
+    [Header("Source")]
+    public GameObject tilePrefab;
+    public Transform backgroundNode;
+    public Transform boardNode;
+    public Transform tetrominoNode;
+
+    [Header("Setting")]
+    [Range(4, 40)]
+    public int boardWidth = 10;
+    //높이 설정
+    [Range(5, 20)]
+    public int boardHeight = 20;
+    //떨어지는 속도
+    public float fallCycle = 1.0f;
+    //위치 보정
+    public float offset_x = 0f;
+    public float offset_y = 0f;
+
+    private int halfWidth;
+    private int halfHeight;
+
+    private float nextFallTime; // 다음에 테트로미노가 떨어질 시간을 저장
+
+    private void Start() // 게임이 시작되고 한 번만 실행
+    {
+        halfWidth = Mathf.RoundToInt(boardWidth * 0.5f);    // 너비의 중간값 설정해주기
+        halfHeight = Mathf.RoundToInt(boardHeight * 0.5f);   // 높이의 중간값 설정해주기
+
+        nextFallTime = Time.time + fallCycle;   // 다음에 테트로미노가 떨어질 시간 설정
+
+        CreateBackground(); // 배경 만들기
+        CreateTetromino(); // 테트로미노 만들기
+    }
+    void Update()   // 매 프레임마다 실행
+    {
+        //초기화
+        Vector3 moveDir = Vector3.zero; //이동 여부 저장용
+        bool isRotate = false;  // 회전 여부 저장용
+
+        //각 키에 따라 이동 여부 혹은 회전 여부를 설정해줍니다.
+        if (Input.GetKeyDown("a"))
+        {
+            moveDir.x = -1;
+
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            moveDir.x = 1;
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            isRotate = true;
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            moveDir.y = -1;
+        }
+
+        // 아무런 키 입력이 없을경우 Tetromino 움직이지 않게하기
+        if (moveDir != Vector3.zero || isRotate)
+        {
+            MoveTetromino(moveDir, isRotate);
+        }
+
+        // 아래로 떨어지는 경우는 강제로 이동시킵니다.
+        if (Time.time > nextFallTime)
+        {
+            nextFallTime = Time.time + fallCycle;   // 다음 떨어질 시간 재설정
+            moveDir.y = -1; //  아래로 한 칸 이동
+            isRotate = false;   // 강제로 이동시 회전 없음
+        }
+
+        // 아무런 키 입력이 없을경우 Tetromino 움직이지 않게하기
+        if (moveDir != Vector3.zero || isRotate)
+        {
+            MoveTetromino(moveDir, isRotate);
+        }
+    }
+
+    // 이동이 가능하면 true, 불가능하면 false를 return
+    bool MoveTetromino(Vector3 moveDir, bool isRotate)
+    {
+        //이동 or 회전 불가시 돌아가기 위한 값 저장
+        Vector3 oldPos = tetrominoNode.transform.position;
+        Quaternion oldRot = tetrominoNode.transform.rotation;
+
+        tetrominoNode.transform.position += moveDir;
+        if (isRotate)
+        {
+            //현재 테트로미노 노드에 90도 회전을 더해 줌.
+            tetrominoNode.transform.rotation *= Quaternion.Euler(0, 0, 90);
+        }
+
+        // 이동 불가시 이전 위치, 회전 으로 돌아가기
+        if (!CanMoveTo(tetrominoNode))
+        {
+            tetrominoNode.transform.position = oldPos;
+            tetrominoNode.transform.rotation = oldRot;
+
+            return false;
+        }
+
+        return true;
+    }
+
+    // 이동 가능한지 체크 후 True or False 반환하는 메서드
+    bool CanMoveTo(Transform root)  //tetrominoNode를 매개변수 root로 가져오기
+    {
+        //tetrominoNode의 자식 타일을 모두 검사
+        for (int i = 0; i < root.childCount; ++i)
+        {
+            var node = root.GetChild(i);
+
+            //유니티 좌표계에서 테트리스 좌표계로 변환
+            int x = Mathf.RoundToInt(node.transform.position.x + halfWidth);
+            int y = Mathf.RoundToInt(node.transform.position.y + halfHeight - 1);
+
+            //이동 가능한 좌표인지 확인 후 반환
+            if (x < 0 || x > boardWidth - 1)
+                return false;
+            if (y < 0)
+                return false;
+
+        }
+        return true;
+    }
+
+    Tile CreateTile(Transform parent, Vector2 position, Color color, int order = 1)
+    {
+        var go = Instantiate(tilePrefab); // tilePrefab를 복제한 오브젝트 생성
+        go.transform.parent = parent; // 부모 지정
+        go.transform.localPosition = position; // 위치 지정
+
+        var tile = go.GetComponent<Tile>(); // tilePrefab의 Tile스크립트 불러오기
+        tile.color = color; // 색상 지정
+        tile.sortingOrder = order; // 순서 지정
+
+        return tile;
+    }
+
+    void CreateBackground()
+    {
+        Color color = Color.gray;   // 배경 색 설정(원하는 색으로 설정 가능)
+
+        // 타일 보드
+        color.a = 0.5f; // 테두리와 구분 위해 투명도 바꾸기
+        for (int x = -halfWidth; x < halfWidth; ++x)
+        {
+            for (int y = halfHeight; y > -halfHeight; --y)
+            {
+                CreateTile(backgroundNode, new Vector2(x, y), color, 0);
+            }
+        }
+
+        // 좌우 테두리
+        color.a = 1.0f;
+        for (int y = halfHeight; y > -halfHeight; --y)
+        {
+            CreateTile(backgroundNode, new Vector2(-halfWidth - 1, y), color, 0);
+            CreateTile(backgroundNode, new Vector2(halfWidth, y), color, 0);
+        }
+
+        // 아래 테두리
+        for (int x = -halfWidth - 1; x <= halfWidth; ++x)
+        {
+            CreateTile(backgroundNode, new Vector2(x, -halfHeight), color, 0);
+        }
+    }
+    void CreateTetromino()
+    {
+        int index = Random.Range(0, 7); // 랜덤으로 0~6 사이의 값 생성
+        //위의 코드 대신 아래와 같은 코드로 원하는 Index모양 확인 가능
+        //int index = 1; 
+
+        Color32 color = Color.white;
+
+        // 회전 계산에 사용하기 위한 쿼터니언 클래스
+        tetrominoNode.rotation = Quaternion.identity;
+        // 테트로미노 생성 위치 (중앙 상단)   
+        tetrominoNode.position = new Vector2(0, halfHeight);
+
+        switch (index)
+        {
+            // 구분을 위해 테트리스 모양에 비슷하게 영어로 표현 (I, J, L ,O, S, T, Z)
+
+            case 0: // I
+                color = new Color32(115, 251, 253, 255);    // 하늘색
+                CreateTile(tetrominoNode, new Vector2(-2f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                break;
+
+            case 1: // J
+                color = new Color32(0, 33, 245, 255);    // 파란색
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(-1f, 1.0f), color);
+                break;
+
+            case 2: // L
+                color = new Color32(243, 168, 59, 255);    // 주황색
+                CreateTile(tetrominoNode, new Vector2(-1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0.0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 1.0f), color);
+                break;
+
+            case 3: // O 
+                color = new Color32(255, 253, 84, 255);    // 노란색
+                CreateTile(tetrominoNode, new Vector2(0f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 1f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 1f), color);
+                break;
+
+            case 4: //  S
+                color = new Color32(117, 250, 76, 255);    // 녹색
+                CreateTile(tetrominoNode, new Vector2(-1f, -1f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, -1f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0f), color);
+                break;
+
+            case 5: //  T
+                color = new Color32(155, 47, 246, 255);    // 자주색
+                CreateTile(tetrominoNode, new Vector2(-1f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 1f), color);
+                break;
+
+            case 6: // Z
+                color = new Color32(235, 51, 35, 255);    // 빨간색
+                CreateTile(tetrominoNode, new Vector2(-1f, 1f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 1f), color);
+                CreateTile(tetrominoNode, new Vector2(0f, 0f), color);
+                CreateTile(tetrominoNode, new Vector2(1f, 0f), color);
+                break;
+        }
+    }
+}
